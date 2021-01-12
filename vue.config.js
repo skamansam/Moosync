fbb91ae9fa006e2de266d59ef36a394642573ace
Ovenoboyo <ovenoboyo@gmail.com>
2021-01-12 13:50:25 +0530

    update vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-    runtimeCompiler: true,
-    pluginOptions: {
-        electronBuilder: {
-            nodeIntegration: true
-        }
-    }
-}
+  runtimeCompiler: true,
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+      disableMainProcessTypescript: false, // Manually disable typescript plugin for main process. Enable if you want to use regular js for the main process (src/background.js by default).
+      mainProcessTypeChecking: false, // Manually enable type checking during webpack bundling for background file.
+    },
+  },
+};
