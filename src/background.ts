10e0460f1ceedeac80d2b4269155c0439d946a5b
Ovenoboyo <ovenoboyo@gmail.com>
2021-01-20 19:20:10 +0530

    New version and default window size

--- src/background.ts
+++ src/background.ts
@@ -13,8 +13,10 @@ protocol.registerSchemesAsPrivileged([{ scheme: 'app', privileges: { secure: tru
 async function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1016,
+    height: 653,
+    minHeight: 653,
+    minWidth: 1016,
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
