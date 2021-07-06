2fc75f7f3f334bc00aac325fd3302b015b28dcbf
ovenoboyo <ovenoboyo@gmail.com>
2021-07-06 18:07:54 +0530

    Use babel-transform-runtime plugin to support electront arget

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  plugins: [
+    ["@babel/plugin-transform-runtime",
+      {
+        "regenerator": true,
+      }]
+  ],
   presets: [
     '@vue/cli-plugin-babel/preset',
     ["@babel/preset-env", {
