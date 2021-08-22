f229b0775fba8f6274d6c4bd395937e0a2b193e2
ovenoboyo <ovenoboyo@gmail.com>
2021-08-23 01:49:09 +0530

    node-keytar requires gnome-keyring and can error if it isnt present

--- src/utils/main/ipc/store.ts
+++ src/utils/main/ipc/store.ts
@@ -21,22 +21,34 @@ export class StoreChannel implements IpcChannelInterface {
 
   private async setKeytar(event: Electron.IpcMainEvent, request: IpcRequest) {
     if (request.params.token && request.params.service) {
-      await keytar.setPassword(request.params.service, os.userInfo().username, request.params.token)
+      try {
+        await keytar.setPassword(request.params.service, os.userInfo().username, request.params.token)
+      } catch (e) {
+        console.error(e)
+      }
     }
     event.reply(request.responseChannel)
   }
 
   private async removeKeytar(event: Electron.IpcMainEvent, request: IpcRequest) {
     if (request.params.service) {
-      await keytar.deletePassword(request.params.service, os.userInfo().username)
+      try {
+        await keytar.deletePassword(request.params.service, os.userInfo().username)
+      } catch (e) {
+        console.error(e)
+      }
     }
     event.reply(request.responseChannel)
   }
 
   private async getKeytar(event: Electron.IpcMainEvent, request: IpcRequest) {
     if (request.params.service) {
-      const token = await keytar.getPassword(request.params.service, os.userInfo().username)
-      event.reply(request.responseChannel, token)
+      try {
+        const token = await keytar.getPassword(request.params.service, os.userInfo().username)
+        event.reply(request.responseChannel, token)
+      } catch (e) {
+        console.error(e)
+      }
     }
     event.reply(request.responseChannel)
   }
