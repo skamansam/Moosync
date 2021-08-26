d3b37e859d0bf5c5e622b2764f6e13678a71f3de
ovenoboyo <ovenoboyo@gmail.com>
2021-08-27 00:56:03 +0530

    Fix autolaunch for linux

--- src/utils/main/autoLaunch.ts
+++ src/utils/main/autoLaunch.ts
@@ -1,8 +1,43 @@
 import { app } from 'electron'
+import { promises as fsP } from 'fs';
+import os from 'os';
+import path from 'path';
 
 export function enableStartup(enabled: boolean) {
-  app.setLoginItemSettings({
-    openAtLogin: enabled,
-    enabled,
-  })
+  if (process.platform === 'win32' || process.platform === 'darwin') {
+    app.setLoginItemSettings({
+      openAtLogin: enabled,
+      enabled,
+    })
+  } else if (process.platform === 'linux') {
+    const directory = path.resolve(os.homedir(), '.config/autostart')
+    const fileName = path.join(directory, `moosync.desktop-${process.env.NODE_ENV}`)
+    if (enabled) {
+      writeLinuxDesktopFile(directory, fileName)
+    } else {
+      removeLinuxDesktopFile(fileName)
+    }
+  }
+}
+
+function removeLinuxDesktopFile(fileName: string) {
+  return fsP.rm(fileName, { force: true })
+}
+
+async function writeLinuxDesktopFile(directory: string, fileName: string) {
+  try {
+    await fsP.access(directory)
+  } catch (e) {
+    await fsP.mkdir(directory, { recursive: true })
+  }
+
+  await fsP.writeFile(fileName,
+    `[Desktop Entry]
+Type=Application
+Version=${app.getVersion()}
+Name=Moosync
+Comment=Moosync music player startup script
+Exec=${process.execPath} ${process.env.NODE_ENV !== 'production' ? path.resolve(process.argv[1]) : ''}
+StartupNotify=false
+Terminal=false`)
 }
