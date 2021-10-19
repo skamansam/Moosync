6f65982bd4299427dbad805d5c53ffd3988bbf09
ovenoboyo <ovenoboyo@gmail.com>
2021-10-20 02:52:48 +0530

    Move giant song icon to left

--- src/mainWindow/components/generic/SongViewCompact.vue
+++ src/mainWindow/components/generic/SongViewCompact.vue
@@ -1,10 +1,10 @@
 <template>
   <b-row no-gutters class="h-100 compact-container">
-    <b-col cols="8" class="h-100 song-list-compact">
-      <SongListCompact :songList="songList" v-on="$listeners" />
-    </b-col>
     <b-col cols="4" class="h-100">
       <SongDetailsCompact :defaultDetails="defaultDetails" :currentSong="currentSong" />
+    </b-col>
+    <b-col cols="8" class="h-100 song-list-compact">
+      <SongListCompact :songList="songList" v-on="$listeners" />
     </b-col>
   </b-row>
 </template>
