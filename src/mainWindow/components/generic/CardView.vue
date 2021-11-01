9cfe6e834617a55ec52c132cbde0dd185e5cd92a
ovenoboyo <ovenoboyo@gmail.com>
2021-11-01 18:44:47 +0530

    Add border radius and box shadow to playlist icon labels

--- src/mainWindow/components/generic/CardView.vue
+++ src/mainWindow/components/generic/CardView.vue
@@ -130,7 +130,10 @@ img
   position: absolute
   top: 16px
   left: 0px
-  padding-left: 10px
+  padding-left: 2px
+  padding-right: 4px
+  border-radius: 0 8px 8px 0
+  box-shadow: 0px 2px 4px 2px rgba(0, 0, 0, 0.2)
   svg
     width: 24px
     height: 24px
