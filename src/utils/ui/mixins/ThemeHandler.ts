13207717932824625a176165465768fd3ff9f40e
Sahil Gupte <sahilsachingupte@gmail.com>
2022-01-06 19:38:35 +0530

    Convert hex to rgb for all theme vars
    Signed-off-by: Sahil Gupte <sahilsachingupte@gmail.com>

--- src/utils/ui/mixins/ThemeHandler.ts
+++ src/utils/ui/mixins/ThemeHandler.ts
@@ -15,20 +15,7 @@ import { vxm } from '../../../mainWindow/store/index';
 export default class ThemeHandler extends Vue {
   private root = document.documentElement
 
-  // static get defaultTheme() {
-  //   return {
-  //     primary: '#212121',
-  //     secondary: '#282828',
-  //     tertiary: '#151515',
-  //     textPrimary: '#ffffff',
-  //     textSecondary: '#565656',
-  //     textInverse: '#000000',
-  //     accent: '#65CB88',
-  //     divider: 'rgba(79, 79, 79, 0.67)',
-  //   }
-  // }
-
-  public async setColorsToRoot(theme: ThemeDetails | undefined) {
+  public setColorsToRoot(theme: ThemeDetails | undefined) {
     const colors = theme?.theme
     if (!colors) {
       this.root.style.cssText = ''
@@ -36,6 +23,25 @@ export default class ThemeHandler extends Vue {
     for (const key in colors) {
       this.root.style.setProperty(`--${key}`, colors[key as keyof ThemeItem])
     }
+
+    this.setRGBValues()
+  }
+
+  private setRGBValues() {
+    const docStyle = getComputedStyle(this.root)
+    const keys = ['primary', 'secondary', 'tertiary', 'textPrimary', 'textSecondary', 'textInverse', 'accent', 'divider']
+    for (const key of keys) {
+      this.root.style.setProperty(`--${key}-rgb`, this.hexToRgb(docStyle.getPropertyValue(`--${key}`).trim()))
+    }
+  }
+
+  private hexToRgb(hex: string) {
+    const arrBuff = new ArrayBuffer(4);
+    const vw = new DataView(arrBuff);
+    vw.setUint32(0, parseInt(hex.replace('#', ''), 16), false);
+    const arrByte = new Uint8Array(arrBuff);
+
+    return arrByte[1] + "," + arrByte[2] + "," + arrByte[3];
   }
 
   public fetchThemeFromID() {
