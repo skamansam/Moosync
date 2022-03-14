60104337adf084e8a847e1eb827c9420c0ce5183
Ovenoboyo <sahilsachingupte@gmail.com>
2022-03-15 02:49:34 +0530

    Auto detech publisher from signing certificate

--- src/mainWindow/App.vue
+++ src/mainWindow/App.vue
@@ -79,6 +79,7 @@ export default class App extends mixins(ThemeHandler, PlayerControls) {
 
   private checkUpdate() {
     window.UpdateUtils.listenUpdate((available) => {
+      console.debug('Got update')
       vxm.themes.isUpdateAvailable = available
     })
 
