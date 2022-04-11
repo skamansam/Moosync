8f20a3f70fe44a55dbf479d2b11d43587bfaeb18
Sahil Gupte <sahilsachingupte@gmail.com>
2022-04-11 13:32:13 +0530

    Export debug_logging when on dev
    Signed-off-by: Sahil Gupte <sahilsachingupte@gmail.com>

--- setup_env.sh
+++ setup_env.sh
@@ -5,4 +5,5 @@ echo SPOTIFYCLIENTSECRET=\'$SPOTIFYCLIENTSECRET\' >> config.env
 echo LASTFMAPIKEY=\'$LASTFMAPIKEY\' >> config.env
 echo LASTFMSECRET=\'$LASTFMSECRET\' >> config.env
 echo FANARTTVAPIKEY=\'$FANARTTVAPIKEY\' >> config.env
+echo DEBUG_LOGGING=\'$DEBUG_LOGGING\' >> config.env
 echo $(ls)
